9db16c8d5d12c1c285e854436c58a5e756b55b42
Yara Jabr <yarajabr2005@gmail.com>
2026-03-06 12:34:17 -0500

    Added controFlow folder and added html and js files to it.

---
+++ controlFlow/control_flow.js
@@ -0,0 +1,51 @@
+let userRole = "admin";
+let accessLevel;
+
+if(userRole==="admin"){
+    accessLevel="Full access granted";
+} else if(userRole==="manager"){
+        accessLevel="Limited access granted";
+    } else {
+        accessLevel="No access granted";
+    }
+
+console.log("Access Level:", accessLevel);
+
+//---
+
+let isLoggedIn = true;
+let userMessage;
+
+if(isLoggedIn){ // isLoggedIn === true
+    if(userRole==="admin"){
+        userMessage="Welcome, admin!";
+    } else{
+        userMessage = "Welcome, User!";
+    }   
+} else { // isLoggedIn === false
+        userMessage = "Please log in to access the system.";
+}
+
+console.log("User Message:", userMessage);
+
+//---
+
+let userType = "subscriber";
+let userCategory;
+
+switch(userType){
+    case "admin":
+        userCategory="Administrator";
+        break;
+    case "manager":
+        userCategory="Manager";
+        break;
+    case "subscriber":
+        userCategory="Subscriber";
+        break;
+    default:
+        userCategory="Unknown";
+}
+
+console.log("User Category:", userCategory);
+
