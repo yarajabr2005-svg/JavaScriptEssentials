a05bc6bc017b6a4a7328efb66b776bcfadd1dc43
Yara Jabr <yarajabr2005@gmail.com>
2026-03-08 22:48:34 +0300

    changed floatParse to parseFloat, and moved the line that displays the result inside the function.

--- calculateArea/calculate_Area.js
+++ calculateArea/calculate_Area.js
@@ -1,11 +1,16 @@
 let length;
 let width;
 
-function calculateArea(){
-    length=floatParse(document.getElementById('length').value);
-    width=floatParse(document.getElementById('width').value);
+function calculateArea() {
+ length = parseFloat(document.getElementById('length').value);
+ width = parseFloat(document.getElementById('width').value);
 
-    let area=length*width;
+ let area=length*width;
+
+  document.getElementById('result').innerText = `The area of the rectangle is: ${area}`;
+
+ 
 }
+
 
  document.getElementById('result').innerText = `The area of the rectangle is: ${area}`;
