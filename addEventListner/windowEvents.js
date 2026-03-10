74207df1cdc5cb2d6aca036617ad129e211ffd79
Yara Jabr <yarajabr2005@gmail.com>
2026-03-10 18:06:04 +0300

    Added the windowEvents html and js files

---
+++ addEventListner/windowEvents.js
@@ -0,0 +1,11 @@
+window.addEventListener('load', function() {
+    console.log('Page and all resources loaded');
+  });
+
+  window.addEventListener('resize', function() {
+    console.log('Window resized');
+  });
+
+  window.addEventListener('scroll', function() {
+    console.log('Document scrolled');
+  });
