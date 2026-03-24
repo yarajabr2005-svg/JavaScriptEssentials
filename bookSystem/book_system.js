718d44ebc8eaf47ed4bf23594a28263494749fe9
Yara Jabr <yarajabr2005@gmail.com>
2026-03-25 01:51:12 +0300

    Added practice task.

--- bookSystem/book_system.js
+++ bookSystem/book_system.js
@@ -28,7 +28,11 @@ function showbooks() {
         <p><strong>Author Name:</strong> ${book.authorName}</p>
         <p><strong>Book Description:</strong> ${book.bookDescription}</p>
         <p><strong>No. of Pages:</strong> ${book.pagesNumber} page(s)</p>
-        <button onclick="editbook(${index})">Edit</button>`
+        <button onclick="editbook(${index})">Edit</button>
+        <button onclick="deletebook(${index})">Delete</button>`
+        //the delete button is for the practice task
+
+
     );
     document.getElementById('books').innerHTML = booksDiv.join('');
 }
@@ -42,6 +46,12 @@ function editbook(index) {
     books.splice(index, 1); // Remove old entry
     showbooks(); // Refresh list
   }
+
+//practice task
+function deletebook(index){
+    books.splice(index,1);
+    showbooks();
+}
 
    function clearInputs() {
             document.getElementById('bookName').value = '';
