f389266735ef40f64f41ffa106ab52ce7d52588c
Yara Jabr <yarajabr2005@gmail.com>
2026-03-26 16:01:48 +0300

    Added the practice task.

--- speedAnalysis/speed_analysis.js
+++ speedAnalysis/speed_analysis.js
@@ -27,6 +27,9 @@ startTime = new Date().getTime();
             var timeElapsed = (endTime - startTime) / 1000; // in seconds
             var userTypedText = document.getElementById("userInput").value;
 
+            //Practice task
+            var totalLength=userTypedText.length;
+
             // Split the text using regex to count words correctly
             var typedWords = userTypedText.split(/\s+/).filter(function (word) {
                 return word !== "";
@@ -41,6 +44,7 @@ startTime = new Date().getTime();
             // Display the results
             var outputDiv = document.getElementById("output");
             outputDiv.innerHTML = "<h2>Typing Test Results:</h2>" +
+                "<p>Total Length: " + totalLength + "</p>" +
                 "<p>Words Typed: " + typedWords + "</p>" +
                 "<p>Time Elapsed: " + timeElapsed.toFixed(2) + " seconds</p>" +
                 "<p>Words Per Minute (WPM): " + wpm + "</p>";
