0638a6fa25fb39d78d409ad0799c09da5d88ccbb
Yara Jabr <yarajabr2005@gmail.com>
2026-03-27 12:54:47 +0300

    Added the practice task.

--- DebugCode/debug_code.js
+++ DebugCode/debug_code.js
@@ -6,7 +6,7 @@
 	// Check if inputs are valid numbers
 	if (!isNaN(num1) && !isNaN(num2)) {
 	// Perform the operation
-					let result = multiply(num1, num2);
+					let result = calculate(num1, num2);
 
 					// Display the result
 					displayResult(result);
@@ -15,17 +15,46 @@
 				}
 			}
 
-			function multiply(a, b) {
+			function calculate(a, b) {
+
+                let result={};
 				// Introduce a debugger statement to pause execution
 				debugger;
 
-				// Multiply the numbers
-				return a * b;
+                result.addition=a+b;
+
+                debugger;
+                
+                result.subtraction=a-b;
+
+                debugger;
+
+                result.multiplication=a*b;
+
+                debugger;
+
+                result.division=a/b;
+
+
+				// Multiply the numbers //Before practice task
+                // it was just      return a*b origionally
+                //Added other operations for practice task
+                debugger;
+				return result;
+
+                //Note: when i used multiple debuggers, i click resume after each
+                //  one and it stops at the next debugger,
+                //  i could just use one and continue seeing the flow using
+                //  the arrows at the top right or F10 buttons and the rest
 			}
 
 			function displayResult(result) {
 				// Display the result in the paragraph element
 				const resultElement = document.getElementById('result');
-				resultElement.textContent = `The result is: ${result}`;
+				resultElement.innerHTML = `<p>The result is:</p> <br>
+                <p>Addition: ${result.addition}<br>
+                   Subtraction ${result.subtraction}<br>
+                   Multiplication: ${result.multiplication}<br>
+                   Division: ${result.division}<br> </p>`;
 			}
 		
